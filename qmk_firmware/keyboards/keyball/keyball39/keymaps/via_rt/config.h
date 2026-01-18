10bd30e806f47cf6c69e911d29711cf08fea1767
takano <moyashi4416@gmail.com>
2026-01-18 22:37:08 +0900

    [200~/*

--- qmk_firmware/keyboards/keyball/keyball39/keymaps/via_rt/config.h
+++ qmk_firmware/keyboards/keyball/keyball39/keymaps/via_rt/config.h
@@ -41,5 +41,5 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #define POINTING_DEVICE_AUTO_MOUSE_ENABLE // 有効化
 #define AUTO_MOUSE_DEFAULT_LAYER 2 // 切り替えるマウスレイヤー番号を指定
-#define AUTO_MOUSE_TIME 1000 // マウスが止まってから元のレイヤーに戻るまでの時間(ms)
+#define AUTO_MOUSE_TIME 500 // マウスが止まってから元のレイヤーに戻るまでの時間(ms)
 #define AUTO_MOUSE_THRESHOLD 40 // どの程度動かしたらマウスレイヤーに切り替えるかの閾値
